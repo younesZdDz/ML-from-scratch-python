4de4d1d6cc66a45660d6db52ddcaf30fbb368261
younesZdDz <32100131+younesZdDz@users.noreply.github.com>
2018-12-21 23:52:00 +0100

    Logistic regression done

--- LogisticRegression/costFunctionReg.py
+++ LogisticRegression/costFunctionReg.py
@@ -1,9 +1,9 @@
-from sigmoid import sigmoid
 import numpy as np
+from scipy.special import expit
 
 def costFunctionReg(theta, reg, XX,y):
     m = y.size
-    h = sigmoid(XX.dot(theta))
+    h = expit(XX.dot(theta))
 
     J = -1 * (1 / m) * (np.log(h).T.dot(y) + np.log(1 - h).T.dot(1 - y)) + (reg / (2 * m)) * np.sum(
         np.square(theta[1:]))

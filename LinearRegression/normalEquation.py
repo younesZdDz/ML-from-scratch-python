5c4498d0ee40f8784707e0bf90994b61e9f27967
younesZdDz <fy_zadi@esi.dz>
2019-02-22 13:06:38 +0100

    linear regression updated

--- LinearRegression/normalEquation.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def normalEquation(X,y):
-    theta = np.zeros(X.shape[1])
-    theta= np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-    return theta
--- LinearRegression/normalizeData.py
+++
@@ -1,3 +0,0 @@
-def normalizeData(X):
-    X= (X - X.mean()) / (X.max() - X.min())
-    return X
